2b9c8f3194e148cc8781ff1d6648ec7a31e4cb87
Carl Pearson <carl.w.pearson@gmail.com>
2020-03-24 08:25:04 -0500

    ci

--- ci/build.sh
+++ ci/build.sh
@@ -3,17 +3,12 @@ set -x -e
 source ci/env.sh
 
 which g++
-which nvcc
 which cmake
 
 g++ --version
-nvcc --version
 cmake --version
 
 mkdir build
 cd build
-cmake .. \
-  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-  -DCMAKE_PREFIX_PATH=$OPENMPI_PATH \
-  -DMPI_HOME=/usr/lib/x86_64-linux-gnu/openmpi/
+cmake ..
 make VERBOSE=1 
